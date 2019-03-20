7f35468cf8029b4968c7b84e7847ee6339229430
Thiago Bonfim <thiakau@gmail.com>
2019-03-20 16:41:21 -0700

    Add colapse panels

--- src/components/Episode/Episode.js
+++ src/components/Episode/Episode.js
@@ -3,34 +3,31 @@ import React from 'react';
 
 export default function Episode({episode}) {
     return ( 
-        <div className="card" >
-            <div className="card-body"> 
-                <div className="card-title row">
-                    <h5 className="col-sm-9">{episode.title}</h5>
-                    <span className="col-sm-3">{episode.seasonOrder} </span>
+        <div className="card col-sm-12" >
+            <div className="card-body row"> 
+                <div className="card-title col-sm-12">
+                    <a href={"#episode-"+ episode.overallOrder} className="row" data-toggle="collapse" aria-expanded="false" aria-controls={"#episode-"+ episode.overallOrder} >
+                        <h5 className="col-sm-9">{episode.title}</h5>
+                        <span className="col-sm-3">{episode.seasonOrder} </span>
+                    </a>
                 </div>
-                
-                <img className="img-fluid rounded" alt={"Image from episode " + episode.title} src={episode.image}/>
-                <ul className="list-group list-group-flush list-unstyled">
-                    <li className="list-group-item">Director: {episode.director}</li>
-                    <li className="list-group-item">Written by: {episode.writter}</li>
-                    <li className="list-group-item">Starring: {episode.starring} </li>
-                    <li className="list-group-item">Release Date: {episode.date} </li>
-                </ul>
-                <p className="card-body">{episode.description}</p>
+                <div className={"collapse " + (episode.overallOrder === 1 ? "show": "" ) } id={"episode-" + episode.overallOrder}>
+                    <img className="img-fluid rounded" alt={"Image from episode " + episode.title} src={episode.image}/>
+                    <ul className="list-group list-group-flush list-unstyled">
+                        <li className="list-group-item">Director: {episode.director}</li>
+                        <li className="list-group-item">Written by: {episode.writter}</li>
+                        <li className="list-group-item">Starring: {episode.starring} </li>
+                        <li className="list-group-item">Release Date: {episode.date} </li>
+                    </ul>
+                    <p className="card-body">{episode.description}</p>
+                </div>
             </div>
-            
         </div>
     )
 }
-
-
-// <div class="card">
-//   <div class="card-body">
-//     This is some text within a card body.
-//   </div>
-// </div>
-
+/* <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+Collapsible Group Item #1
+</button> */
 
 // <ul class="list-group list-group-flush">
 //     <li class="list-group-item">Cras justo odio</li>
