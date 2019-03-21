ca997ffb2a5839062b392a03117b5f606a2fdba1
Thiago Bonfim <thiakau@gmail.com>
2019-03-21 15:47:18 -0700

    Adding page title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import EpisodeList from './components/EpisodeList/EpisodeList';
 import './App.css';
 
 export default function App () {
-  return <EpisodeList />;
+  return (
+    <div className="mt-5">
+      <h1 className="text-info">Black Mirror</h1>
+      <h4 className="text-secondary">Episodes List</h4>
+      <EpisodeList />
+    </div>
+  );
   
 }
--- src/components/Episode/Episode.js
+++ src/components/Episode/Episode.js
@@ -6,7 +6,7 @@ export default function Episode({episode}) {
         <div className="card col-sm-12" >
             <div className="card-body row"> 
                 <div className="card-title col-sm-12">
-                    <a href={"#episode-"+ episode.overallOrder} className="row" data-toggle="collapse" aria-expanded="false" aria-controls={"#episode-"+ episode.overallOrder} >
+                    <a href={"#episode-"+ episode.overallOrder} className="row text-secondary" data-toggle="collapse" aria-expanded="false" aria-controls={"#episode-"+ episode.overallOrder} >
                         <h5 className="col-sm-9">{episode.title}</h5>
                         <span className="col-sm-3">{episode.seasonOrder} </span>
                     </a>
@@ -25,21 +25,3 @@ export default function Episode({episode}) {
         </div>
     )
 }
-/* <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-Collapsible Group Item #1
-</button> */
-
-// <ul class="list-group list-group-flush">
-//     <li class="list-group-item">Cras justo odio</li>
-//     <li class="list-group-item">Dapibus ac facilisis in</li>
-//     <li class="list-group-item">Vestibulum at eros</li>
-//   </ul>
-        // "overallOrder": 1,
-        // "seasonOrder": "Season 1 - Episode 1",
-        // "title": "The National Anthem",
-        // "director": "Otto Bathurst",
-        // "writer": "Charlie Brooker",
-        // "date": "4 December 2011",
-        // "ukViewers": "2.07",
-        // "description": "Princess Susannah, a member of the British royal family, has been kidnapped. To return the Princess, the kidnapper demands that the country's Prime Minister, Michael Callow, has sexual intercourse with a pig on live television, otherwise he will kill her. Callow refuses to be blackmailed, but public opinion is gradually turning against him. Security forces are trying to locate the kidnapper, but every effort fails. Under pressure, Callow finally agrees to fulfill the demand. The British people gather in public places and watch the shocking broadcast, with no one realising that the kidnapper has already released the Princess, thirty minutes before Callow's sex act began. The kidnapper commits suicide by hanging and he is later revealed to be an artist, who planned the debacle in order to make a statement on people's obsession with the media. One year later, Callow's approval ratings have fully recovered, but his relationship with his wife has suffered a heavy blow.",
-        // "starring" : ". . ."
