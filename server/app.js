842e1f8d69ed324e99c2459ed00a3141d37c4bd4
Thiago Bonfim <thiakau@gmail.com>
2019-04-18 14:33:23 -0700

    Adding nodemon and error handling

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const episodeRoutes = require('./api/routes/episodes');
 
+app.use(morgan('dev'));
+
 app.use('/episodes', episodeRoutes);
+
+
+app.use((req, res, next) => {
+  const error = new Error('Path not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message || "Undefined Error!"
+    }
+  });
+});
+
 
 module.exports = app;
